1e38078b3e376755cca365815dd3b19aed3e33a5
Roman Tolstosheyev <izobretatel777@mail.ru>
2021-02-22 09:15:03 +0400

    lesson 1

---
+++ Lesson-1/Roman - Hypotenuse of right triangle.py
@@ -0,0 +1,5 @@
+from math import sqrt
+a = float(input('Enter first cathetus: '))
+b = float(input('Enter second cathetus: '))
+c = sqrt(a ** 2 + b ** 2)
+print('The hypotenuse of given triangle is', c)
---
+++ Lesson-1/Roman - Sum and average of three numbers.py
@@ -0,0 +1,4 @@
+a, b, c = map(int, input('Enter three integers in one line: ').split())
+s = a + b + c
+print('Sum of numbers:', s)
+print('Average of numbers:', s / 3)
