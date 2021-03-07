5b089d15627b8fda966d43c431952eb3e4cc3385
Roman Tolstosheyev <izobretatel777@mail.ru>
2021-03-07 17:34:20 +0400

    lesson 5 updated

--- Lesson-5/Automorphic number_v2.py
+++ Lesson-5/Automorphic number_v2.py
@@ -1,16 +1,16 @@
-import math
-
 while True:
     n = int(input('Enter natural number: '))
     if n <= 0:
         print('Error, try again')
     else:
         break
-
-print(f'Automorphic numbers from 1 to {n} are:')
 for i in range(1, n + 1):
-    l = math.floor(math.log10(i)) + 1
-    t = i * i
-    if t % 10 ** l == i:
+    cur = i
+    square = i * i
+    while cur > 0:
+        if cur % 10 != square % 10:
+            break
+        square //= 10
+        cur //= 10
+    if cur == 0:
         print(f'{i}*{i}={i * i}')
-
